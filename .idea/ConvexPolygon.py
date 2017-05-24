a4a62279f97cbfe8df31b8ec1d56de2e72a8fa88
NachoChef <hock3ydud3@gmail.com>
2017-05-24 16:33:09 -0500

    cleaned up imports

--- .idea/ConvexPolygon.py
+++ .idea/ConvexPolygon.py
@@ -1,16 +1,12 @@
 import math as m
 import matplotlib.pyplot as plt
-import matplotlib
-from matplotlib.patches import Polygon
-from matplotlib.collections import PatchCollection
-from matplotlib import pyplot
 
 class ConvexPolygon:
     def __init__(self, c : tuple, r : int):
         self.center_point = c
         self.radius = r
-        self.x_points = [(self.center_point[0] + self.radius * m.cos(x)) for x in self.drange(0, 2 * m.pi, 0.1)]
-        self.y_points = [(self.center_point[1] + self.radius * m.sin(y)) for y in self.drange(0, 2 * m.pi, 0.1)]
+        self.x_points = [(self.center_point[0] + self.radius * m.cos(x)) for x in self.drange(0, 2 * m.pi, 0.05)]
+        self.y_points = [(self.center_point[1] + self.radius * m.sin(y)) for y in self.drange(0, 2 * m.pi, 0.05)]
 
     def drange(self, start, stop, step):
         tmp = start
