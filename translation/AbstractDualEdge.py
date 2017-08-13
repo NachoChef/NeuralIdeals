0c645ea3714becf2661655afb279c0a2bd2e91e1
Justin Jones <jxj037@shsu.edu>
2017-08-13 12:55:51 -0500

    Cleanup

--- translation/AbstractDualEdge.py
+++
@@ -1,7 +0,0 @@
-class AbstractDualEdge:
-    def __init__(self, fr : AbstractDualNode, to : AbstractDualNode, label : AbstractCurve):
-        self.fr = fr
-        self.to = to
-        self.label = label
-    #end init
-        
--- translation/DecompositionStep.py
+++
@@ -1,5 +0,0 @@
-#package icircles.decomposition
-
-#tremap, map?
-
-class DecompositionStep:
